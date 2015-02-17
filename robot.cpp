1c3744822a3235a1f486b209f575933a06d46c32
Adam Goldsmith <adam@adamgoldsmith.name>
2015-02-16 21:46:28 -0500

    add acceleration to drivetrain (X axis not used))

--- robot.cpp
+++ robot.cpp
@@ -1,4 +1,7 @@
 #include "WPILib.h"
+#include <cmath>
+
+#define RAMP_RATE 0.05
 
 class Robot : public IterativeRobot
 {
@@ -34,8 +37,23 @@ public:
 	void TeleopPeriodic()
 	{
 		//Driving
+		static float last[3];
+		static double scaled[3];
 		float throttleScale = ((1 - driveStick.GetThrottle()) / 2);
-		drivetrain.MecanumDrive_Cartesian(driveStick.GetX()*throttleScale, driveStick.GetY()*throttleScale, driveStick.GetTwist()*throttleScale*driveStick.GetRawButton(2));
+		scaled[0] = driveStick.GetX()*throttleScale;
+		scaled[1] = driveStick.GetY()*throttleScale;
+		scaled[2] = driveStick.GetTwist()*throttleScale*driveStick.GetRawButton(2);		
+
+		last[0] = (std::abs(last[0] - scaled[0]) < RAMP_RATE ? scaled[0] :
+		           scaled[0] > last[0] ? last[0] + RAMP_RATE :
+		           scaled[0] < last[0] ? last[0] - RAMP_RATE : last[0]);
+		last[1] = (std::abs(last[0] - scaled[1]) < RAMP_RATE ? scaled[1] :
+		           scaled[1] > last[1] ? last[1] + RAMP_RATE :
+		           scaled[1] < last[1] ? last[1] - RAMP_RATE : last[1]);
+		last[2] = (std::abs(last[0] - scaled[2]) < RAMP_RATE ? scaled[2] :
+		           scaled[2] > last[2] ? last[2] + RAMP_RATE :
+		           scaled[2] < last[2] ? last[2] - RAMP_RATE : last[2]);
+		drivetrain.MecanumDrive_Cartesian(scaled[0], last[1], last[2]);
 	}
 };
 
